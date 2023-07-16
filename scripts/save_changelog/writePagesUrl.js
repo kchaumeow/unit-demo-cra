5cb93f9a5c0cd00061a65c5f4e7b337c0bb2f5a9
kchaumeow <atrofimova516@gmail.com>
2023-07-16 20:40:52 +0300

    fix: writePagesUrl

--- scripts/save_changelog/writePagesUrl.js
+++ scripts/save_changelog/writePagesUrl.js
@@ -1,15 +1,26 @@
 const GitHubAPI = require("./gitHubAPI");
+const runCommand = require("./runCommand");
 
 async function writePagesUrl() {
   const token = process.argv[2];
   const [owner, repo] = process.argv[3].split("/");
-  const gitHubAPI = new GitHubAPI(token, owner, repo);
   const ghPagesUrl = process.argv[4];
-  const currentRepoIssue = (await gitHubAPI.fetchRepoIssues())[-1];
+  const gitHubAPI = new GitHubAPI(token, owner, repo);
+
+  const currentReleaseTag = await runCommand(
+    `sh ${__dirname}/get_current_release_tag.sh`
+  );
+
+  const issueTitle = `Release ${currentReleaseTag}`;
+
+  const repoIssues = (await gitHubAPI.fetchRepoIssues()).data;
+  const currentIssue = repoIssues.find((issue) => issue.title === issueTitle);
+
   await gitHubAPI.updateIssue({
-    issueNumber: currentRepoIssue.number,
-    title: currentRepoIssue.title,
-    body: currentRepoIssue.body + '\n' + `Deployment: ${ghPagesUrl}`,
+    issueNumber: currentIssue.number,
+    title: currentIssue.title,
+    body: currentIssue.body + "\n" + `Deployment: ${ghPagesUrl}`,
     labels: ["RELEASE"],
   });
 }
+writePagesUrl();
